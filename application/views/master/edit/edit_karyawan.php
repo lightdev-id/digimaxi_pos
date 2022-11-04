a7530bed8246652543c6df0528382d68bbe65ff7
Rafii Yuuki 銀 <92235855+SulthanRaghib@users.noreply.github.com>
2022-11-04 15:17:50 +0700

    fix: typo edit karyawan

--- application/views/master/edit/edit_karyawan.php
+++ application/views/master/edit/edit_karyawan.php
@@ -1,7 +1,7 @@
 <div class="container">
    <hr>
    <div class="col-12 d-flex flex-row align-items-center justify-content-between pl-0">
-      <h5 class="mb-0">Tambah Data Karyawan</h5>
+      <h5 class="mb-0">Edit Data Karyawan</h5>
       <button type="button" class="btn btn-sm btn-warning float-right" data-toggle="modal" data-target="#batal">
          <i class="fa-solid fa-xmark pr-2"></i>Batal
       </button>
